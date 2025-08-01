f9d1e3c217da3480b918b4d70f26cf7932e5c38a
Tamer bali <tamerinale@gmail.com>
2025-08-01 11:41:24 +0100

    Make a creative landing page

---
+++ iphone.js
@@ -0,0 +1,14 @@
+// تعريف بالمتغيرات
+let img = document.querySelector(".img")
+let container = document.querySelector(".container");
+
+// تغير جهاز IPONE
+function phones(phone) {
+    img.src= phone
+}
+// تغيير لون الخلفية
+function colors(color){
+    container.style.background = color
+    
+}
+
